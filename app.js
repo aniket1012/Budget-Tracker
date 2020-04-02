dadd13953697cabc7d6d11cea0f9d3e60989a0f5
Aniket <aniketupatel1@gmail.com>
2020-04-02 14:01:42 -0400

    added event listener to done button and enter key

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
+ //BUDGET CONTROLLER 
+ 
  let budgetController = (function(){
 
-    let x = 23
 
-    let add  = function(a) {
-        return x + a 
-    }
-
-    return  {
-        publicTest: function(b) {
-            return add(b);
-            
-        }
-    }
  })()
 
 
-
+// UI CONTROLLER 
  let UIController = (function(){
 
  })()
 
+
+
+
+//GLOBAL APP CONTROLLER
  let controller = (function(budgetCtrl, UICtrl){
 
-    let z = budgetCtrl.publicTest(5)
+    let ctrlAddItem = function() {
+        // 1. Get field input data
+        
 
-    return {
-        anotherPublic: function(){
-            console.log(z);
-            
-        }
+        //2. Add the item to budget controller
+
+        //3. Add the item to the UI
+
+        //4. Calculate the budget 
+
+        //5. Display the budget on the UI 
+
+        console.log('it Works ');
+        
     }
+
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+
+    document.addEventListener('keypress', function(e){
+        if(e.keyCode === 13 || e.which === 13){
+            ctrlAddItem()
+        }
+        
+    })
 
  })(budgetController,UIController)
