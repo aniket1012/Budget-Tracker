4892394c393fdf574fb807398dd4e353d53782c2
Aniket <aniketupatel1@gmail.com>
2020-04-03 14:07:11 -0400

    updated DOM with budget object data

--- app.js
+++ app.js
@@ -32,7 +32,7 @@
             inc: 0
         },
         budget: 0,
-        percenttage: -1
+        percentage: -1
     }
 
     return {
@@ -69,11 +69,11 @@
             //Caclualte the budget: income - expenses 
 
             data.budget = (data.totals.inc - data.totals.exp)
-            //Calculate the percenttage of income we spent
+            //Calculate the percentage of income we spent
             if(data.totals.inc > 0) {
-                data.percenttage = Math.round((data.totals.exp / data.totals.inc) * 100)
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100)
             } else {
-                data.percenttage = -1
+                data.percentage = -1
             }
         },
 
@@ -82,7 +82,7 @@
                 budget: data.budget,
                 totalInc: data.totals.inc,
                 totalExp: data.totals.exp,
-                percenttage: data.percenttage
+                percentage: data.percentage
             }
         },
 
@@ -106,7 +106,12 @@
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage',
+
         
     }
 
@@ -156,6 +161,20 @@
 
         },
 
+        displayBudget: function(obj){
+
+            document.querySelector(DOMStrings.budgetLabel).textContent = obj.budget
+            document.querySelector(DOMStrings.incomeLabel).textContent = obj.totalInc
+            document.querySelector(DOMStrings.expensesLabel).textContent = obj.totalExp
+           
+
+            if(obj.percentage > 0) {
+                 document.querySelector(DOMStrings.percentageLabel).textContent = obj.percentage + '%'
+            } else {
+                document.querySelector(DOMStrings.percentageLabel).textContent = '---'
+            }
+        },
+
         getDOMStrings: function(){
             return DOMStrings
         }
@@ -190,7 +209,7 @@
         //2. Return the Budget 
         let budget = budgetCtrl.getBudget()
         //3. Display the budget on the UI 
-        console.log(budget);
+        UICtrl.displayBudget(budget)
         
     }
 
@@ -215,7 +234,13 @@
 
     return {
         init: function(){
-            console.log("app has started");
+            console.log("Application has started");
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            })
             setupEventListeners()
             
         }
